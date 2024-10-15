0bebbb4ec202494d19cd8b0226ddaa68445b8387
GuilhermeGuerraCortelini <153189908+GuilhermeGuerraCortelini@users.noreply.github.com>
2024-10-15 09:17:56 -0300

    Aula 15/10 - Status Request

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/somar', function(Request $request) {
+    // Não está chegando nada pela request
+    if(count($request->all()) < 1){
+        return response ()->json([
+            'message' => 'Não há valores para somar.'
+        ], 406);
+    }
+    
+    $soma = array_sum($request->all());
+    return response()->json([
+        'message' => 'Somado com sucesso', // Opcional
+        'sum' => $soma,
+    ]);
+});
